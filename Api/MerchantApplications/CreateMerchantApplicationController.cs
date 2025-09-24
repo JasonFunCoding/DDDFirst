1dda7c516fa8e997bd7d43f76ce608d9de2ed260
Jason <xxhtristan@gmail.com>
2025-09-24 15:18:57 +0200

    Add files via upload

---
+++ Api/MerchantApplications/CreateMerchantApplicationController.cs
@@ -0,0 +1,33 @@
+using JetBrains.Annotations;
+using MediatR;
+using Qliro.FunToCode.Application.Contracts;
+using Qliro.FunToCode.Application.MerchantApplications.UseCases;
+using Qliro.FunToCode.Application.MerchantApplications.UseCases.Create;
+
+namespace Qliro.FunToCode.Api.MerchantApplications;
+
+[UsedImplicitly]
+public class CreateMerchantApplicationController : IEndpoint
+{
+  public static void MapEndpoint(IEndpointRouteBuilder endpoints)
+  {
+    endpoints.MapPost("/merchant-applications",
+      async (CreateMerchantApplicationRequest request, IMediator mediator, CancellationToken ct) =>
+      {
+        var command = new CreateMerchantApplicationCommand(request.Name, request.Email);
+
+        var response = await mediator.Send(command, ct);
+          if (response == null)
+          {
+              Results.BadRequest();
+          }
+
+          return response != null
+          ? Results.Ok(response)
+          : Results.BadRequest();
+      })
+      .WithName("Create Merchant Application")
+      .WithTags("Merchant Application")
+      .WithOpenApi();
+  }
+}
---
+++ Application/MerchantApplications/MerchantApplicationCreatedHandler2.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Qliro.FunToCode.Application.Contracts;
+using Qliro.FunToCode.Domain.MerchantApplications.Events;
+
+namespace Qliro.FunToCode.Application.MerchantApplications.Handlers;
+
+public class MerchantApplicationCreatedHandler2(IEmailSender emailSender) : INotificationHandler<MerchantApplicationCreatedEvent>
+{
+
+    public async Task Handle(MerchantApplicationCreatedEvent @event, CancellationToken cancellationToken)
+    {
+      //emailSender.SendApplicationUrl(@event.Email, $"https://nocode.platform.com/{@event.Id}");
+       throw new NotImplementedException();
+
+
+
+        _ = Task.Run(() =>
+        {
+            emailSender.SendApplicationUrl(@event.Email, $"https://nocode.platform.com/{@event.Id}");
+            throw new NotImplementedException();
+        });
+    }
+}
